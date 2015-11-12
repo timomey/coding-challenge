140e9a6ef056160cf44014340160e62deafa0c03
Timo Meyer <timomey@gmail.com>
2015-11-12 11:51:18 -0800

    tweets_cleaned works
    tweets cleaned works and keeps track of unicode as well as tweets that
    were left out because there is data missing

--- src/tweets_cleaned.py
+++ src/tweets_cleaned.py
@@ -7,16 +7,53 @@
 
 import json
 
-tweetfile_handle = open('../tweet_input/tweets.txt')
 
-#test: only take one line
-oneline = tweetfile_handle.readline()
-#use json.loads to turn this line to a dictionary for easy access (alternative would be using reg.exp. - check speed)
-oneline_dict = json.loads(oneline)
-text = oneline_dict['text']
+def clean_string(text):
+    text.strip()
+    text.replace("\/", "/")
+    text.replace("\\", "\ ")
+    text.replace("\'", "'")
+    text.replace('\"', '"')
+    text.replace("\n", " ")
+    text.replace("\t", " ")
+    text = " ".join(text.split())
+    return text
 
+numberunicode = 0
+numberleftout = 0
+#open tweets file; note that "with ... as ..." closes file connection automatically
+with open('../tweet_input/tweets.txt','r') as tweetfile_handle:
+    #only take one line at a time
+    for oneline in tweetfile_handle:
+        #use json.loads to turn this line to a dictionary for easy access (alternative would be using reg.exp. - check speed)
+        oneline_dict = json.loads(oneline)
+        #from the json, the dictionary 'text' entry has the tweet
+        try:
+            text_u = oneline_dict['text']
+            #convert it to ascii, ignoring non readable stuff. (TODO RECORD IF IT IS NOT READABLE)
+            #text_ascii = text_u.encode('ascii','ignore')
+            try:
+                text_ascii = text_u.encode('ascii')
+            except UnicodeEncodeError:
+                text_ascii = text_u.encode('ascii','ignore')
+                numberunicode += 1
 
+            timestamp = oneline_dict['created_at']
+            timestamp = timestamp.encode('ascii')
+            cleantext = clean_string(text_ascii)
+            result = cleantext + ' (timestamp: ' + timestamp + ')'
 
-#go through entire file
-#for line in tweetfile_handle:
-#    print line.split(',"text":"')[1].split('","source')[0]
+            #tweet_out = open('../tweet_output/ft1.txt','a')
+            with open('../tweet_output/ft1.txt', 'a') as tweet_out:
+                tweet_out.write(result)
+                tweet_out.write('\n')
+        except KeyError:
+            numberleftout += 1
+
+unicodeleftout = str(numberunicode) + ' tweets contained unicode.'
+keyerrorcount = str(numberleftout) + ' tweets left out because of missing data.'
+print unicodeleftout
+print keyerrorcount
+with open('../tweet_output/ft1.txt', 'a') as tweet_out:
+    tweet_out.write('\n')
+    tweet_out.write(unicodeleftout)
